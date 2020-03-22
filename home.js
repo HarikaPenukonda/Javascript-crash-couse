568aad6f34a7873f95aae5f1e0ea76e9b2937c6c
harika penukonda <penukondaharika@gmail.com>
2020-03-21 20:47:08 -0400

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+//alert('Welcome Harika');
+
+//variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 24;
+console.log(someNumber);
+
+document.getElementById('someText').innerHTML = 'Hey There';
