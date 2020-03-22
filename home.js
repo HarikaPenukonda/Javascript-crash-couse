8d03503e40b4a2ae71708f25b4925a052810dc34
harika penukonda <penukondaharika@gmail.com>
2020-03-21 20:53:06 -0400

    Update home.js

--- home.js
+++ home.js
@@ -10,3 +10,8 @@ var someNumber = 24;
 console.log(someNumber);
 
 document.getElementById('someText').innerHTML = 'Hey There';
+
+var age = prompt('What is your age?');
+
+document.getElementById('someText').innerHTML = age;
+
